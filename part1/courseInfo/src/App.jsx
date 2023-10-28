9527038086f9d67986cbde893f16c41c31c3915a
Sanjib Poon <sanjibpoon123@gmail.com>
2023-10-28 14:33:58 +0545

    Part1b completion

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,23 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const contents = [
-    { id: 0, part: part1, exercises: exercises1 },
-    { id: 1, part: part2, exercises: exercises2 },
-    { id: 2, part: part3, exercises: exercises3 }
-  ];
+  const course = {
+    name: 'Half Stack app dev',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <Header heading={course} />
-      <Content contents={contents} />
-      <p>Total no. of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header heading={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -26,10 +30,17 @@ const Header = ({ heading }) => {
   return <h1>{heading}</h1>
 }
 
-const Content = ({ contents }) => {
-  return contents.map((content) =>
-    <p key={content.id}>{content.part} : {content.exercises}</p>
+const Content = ({ parts }) => {
+  return parts.map((content) =>
+    <p key={content.id}>{content.name} : {content.exercises}</p>
   )
+}
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((total, currentPart) => {
+    return total + currentPart.exercises
+  }, 0);
+  return <p>Total Exercises: {totalExercises}</p>
 }
 
 export default App;
